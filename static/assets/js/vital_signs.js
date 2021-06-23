0fe46aa4da9c4e13bd38f4de7d41d8276d8fb3a9
Fernando Huanca <fjhuanca@uc.cl>
2021-06-23 11:03:43 -0400

    wss fixes

--- static/assets/js/vital_signs.js
+++ static/assets/js/vital_signs.js
@@ -1,6 +1,6 @@
 let socket_signs = new WebSocket(
     (window.location.protocol == 'https:' ? 'wss://' : 'ws://')
-    + 'iee2913-g10-project.southcentralus.cloudapp.azure.com:8001/ws/signs');
+    + 'iee2913-g10-project.southcentralus.cloudapp.azure.com/wss/signs');
 
 // socket.onopen = function(e){
 //     alert('Conectado Exitosamente');
