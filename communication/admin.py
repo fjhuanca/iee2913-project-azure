ce3bead2222f33695dd093caf2ef16ad804a9894
Fernando Huanca <fjhuanca@uc.cl>
2021-07-21 15:45:14 -0400

    admin support

--- communication/admin.py
+++ communication/admin.py
@@ -1,3 +1,10 @@
+from communication.views import MessagesJsons
 from django.contrib import admin
 
 # Register your models here.
+from .models import Messages, DrugRecords, EvacuationRecords, FoodRecords
+# Register your models to admin site, then you can add, edit, delete and search your models in Django admin site.
+admin.site.register(Messages)
+admin.site.register(DrugRecords)
+admin.site.register(EvacuationRecords)
+admin.site.register(FoodRecords)
