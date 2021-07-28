48da7c31f4eeca09b303c820f8ffd853ce358aa2
Fernando Huanca <fjhuanca@uc.cl>
2021-07-28 02:52:51 -0400

    working on audio

--- HealthCareServer/settings.py
+++ HealthCareServer/settings.py
@@ -153,9 +153,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = 'static'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
                     #os.path.join(BASE_DIR, 'HealthCareServer', 'static'),
-                    )
+                    # )
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
 #     '/var/www/static/',
