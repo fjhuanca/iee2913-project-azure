34f86596e43967d3b6c72109bc38abb8b562823a
Fernando Huanca <fjhuanca@uc.cl>
2021-07-28 19:58:27 -0400

    led button

--- static/assets/js/led_on_off.js
+++ static/assets/js/led_on_off.js
@@ -7,7 +7,7 @@ function startLedSocket(){
     //     (window.location.protocol == 'https:' ? 'ws://' : 'ws://')
     //     + 'localhost:8000/wss/receiver/led/');
         
-    let socket = new WebSocket(
+    let socket2 = new WebSocket(
             (window.location.protocol == 'https:' ? 'wss://' : 'wss://')
                 + 'iee2913-g10-project.southcentralus.cloudapp.azure.com/wss/receiver/led/');
             // Send to Everyone on channel test123
