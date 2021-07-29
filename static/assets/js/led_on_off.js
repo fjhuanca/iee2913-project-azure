f9f4ab7bae45182ff43bada12b7af2c676624de3
Fernando Huanca <fjhuanca@uc.cl>
2021-07-28 20:46:05 -0400

    led button

--- static/assets/js/led_on_off.js
+++ static/assets/js/led_on_off.js
@@ -7,13 +7,25 @@ function startLedSocket(){
     //     (window.location.protocol == 'https:' ? 'ws://' : 'ws://')
     //     + 'localhost:8000/wss/receiver/led/');
         
-    let socket2 = new WebSocket(
+    socket2 = new WebSocket(
             (window.location.protocol == 'https:' ? 'wss://' : 'wss://')
                 + 'iee2913-g10-project.southcentralus.cloudapp.azure.com/wss/receiver/led/');
+
+    socket3 = new WebSocket(
+                    (window.location.protocol == 'https:' ? 'wss://' : 'wss://')
+                        + 'iee2913-g10-project.southcentralus.cloudapp.azure.com/wss/sender/led/');
+    socket3.onmessage = function(e){
+        // console.log(e.data);
+        var recData = e.data;
+        isOn = parseInt(e.data);
+        if (isOn) $('#toggle').bootstrapToggle("on");
+        else $('#toggle').bootstrapToggle("off");
+    };
             // Send to Everyone on channel test123
-    t = setInterval(function() {
+    const checkbox = document.getElementById("toggle");
+    checkbox.onchange = function (event){
         val =  ($('#toggle').prop('checked')) ? 1: 0;
-        socket2.send(JSON.stringify({status: val}));
-        console.log("here");
-    }, 5000 );
+        if (socket2.status) socket2.send(JSON.stringify({status: val}));
+        // console.log("here");
+    };
 }
