48357a58e370bc4598af854920e11e703a2bad83
Fernando Huanca <fjhuanca@uc.cl>
2021-08-04 18:47:48 -0400

    server display update

--- static/assets/js/ecg_plot.js
+++ static/assets/js/ecg_plot.js
@@ -7,14 +7,55 @@ socket.onopen = function(e){
     alert('Conectado Exitosamente');
 };
 
+ecg_val = {
+    valueInternal: -1,
+    valueListener: function(val) {},
+    set value(val) {
+      this.pastValue = this.valueInternal;
+      this.valueInternal = val;
+      this.valueListener(val);
+    },
+    get value() {
+      return this.valueInternal;
+    },
+    onchange: function(listener) {
+      this.valueListener = listener;
+    }
+  };
+
+var isTabActive;
+
+  window.onfocus = function () { 
+    isTabActive = true; 
+  }; 
+  
+  window.onblur = function () { 
+    isTabActive = false; 
+}; 
+
+ecg_val.onchange(function(val){
+    var status_ecg = document.getElementById("status-ecg")
+    if (val >= 0 && ecg_val.pastValue < 0){
+        status_ecg.className = "text-success fw-bold";
+        status_ecg.textContent = "Monitoreando";
+    }
+    else if (val < 0){
+        status_ecg.className = "text-danger fw-bold";
+        status_ecg.textContent = "Esperando";
+    }
+});
+
 socket.onmessage = function(e){
     // console.log(e.data);
     var recData = JSON.parse(e.data);
-    dataObjNew = dataObj['data']['datasets'][0]['data'];
-    dataObjNew.shift();
-    dataObjNew.push(recData.value);
-    dataObj['data']['datasets'][0]['data'] = dataObjNew;
-    window.myLine.update();
+    ecg_val.value = recData.value;
+    if (ecg_val.value >= 0 && isTabActive){
+        dataObjNew = dataObj['data']['datasets'][0]['data'];
+        dataObjNew.shift();
+        dataObjNew.push(recData.value);
+        dataObj['data']['datasets'][0]['data'] = dataObjNew;
+        window.myLine.update();
+    }
 };
 
 socket.onclose = function(e){
--- static/assets/js/vital_signs.js
+++ static/assets/js/vital_signs.js
@@ -6,13 +6,156 @@ let socket_signs = new WebSocket(
 //     alert('Conectado Exitosamente');
 // };
 
+bpm = {
+    valueInternal: -1,
+    valueListener: function(val) {},
+    set value(val) {
+      this.pastValue = this.valueInternal;
+      this.valueInternal = val;
+      this.valueListener(val);
+    },
+    get value() {
+      return this.valueInternal;
+    },
+    onchange: function(listener) {
+      this.valueListener = listener;
+    }
+  };
+
+bpm.onchange(function(val){
+    var status_bpm = document.getElementById("status-bpm")
+    if (val >= 0 && bpm.pastValue < 0){
+        status_bpm.className = "text-success fw-bold";
+        status_bpm.textContent = "Monitoreando";
+    }
+    else if (val < 0){
+        status_bpm.className = "text-danger fw-bold";
+        status_bpm.textContent = "Esperando";
+    }
+});
+
+bf = {
+    valueInternal: -1,
+    valueListener: function(val) {},
+    set value(val) {
+      this.pastValue = this.valueInternal;
+      this.valueInternal = val;
+      this.valueListener(val);
+    },
+    get value() {
+      return this.valueInternal;
+    },
+    onchange: function(listener) {
+      this.valueListener = listener;
+    }
+  };
+
+bf.onchange(function(val){
+    var status_bf = document.getElementById("status-bf")
+    if (val >= 0 && bf.pastValue < 0){
+        status_bf.className = "text-success fw-bold";
+        status_bf.textContent = "Monitoreando";
+    }
+    else if (val < 0){
+        status_bf.className = "text-danger fw-bold";
+        status_bf.textContent = "Esperando";
+    }
+});
+
+
+temp = {
+    valueInternal: -1,
+    valueListener: function(val) {},
+    set value(val) {
+      this.pastValue = this.valueInternal;
+      this.valueInternal = val;
+      this.valueListener(val);
+    },
+    get value() {
+      return this.valueInternal;
+    },
+    onchange: function(listener) {
+      this.valueListener = listener;
+    }
+  };
+
+temp.onchange(function(val){
+    var status_temp = document.getElementById("status-temp")
+    if (val >= 0 && temp.pastValue < 0){
+        status_temp.className = "text-success fw-bold";
+        status_temp.textContent = "Monitoreando";
+    }
+    else if (val < 0){
+        status_temp.className = "text-danger fw-bold";
+        status_temp.textContent = "Esperando";
+    }
+});
+
+spo = {
+    valueInternal: -1,
+    valueListener: function(val) {},
+    set value(val) {
+      this.pastValue = this.valueInternal;
+      this.valueInternal = val;
+      this.valueListener(val);
+    },
+    get value() {
+      return this.valueInternal;
+    },
+    onchange: function(listener) {
+      this.valueListener = listener;
+    }
+  };
+
+spo.onchange(function(val){
+    var status_spo = document.getElementById("status-spo")
+    if (val >= 0 && spo.pastValue < 0){
+        status_spo.className = "text-success fw-bold";
+        status_spo.textContent = "Monitoreando";
+    }
+    else if (val < 0){
+        status_spo.className = "text-danger fw-bold";
+        status_spo.textContent = "Esperando";
+    }
+});
+
+// NAME.className="mynewclass"
 socket_signs.onmessage = function(e){
     // console.log(e.data);
     var recData = JSON.parse(e.data);
-    document.getElementById("bpm_inst").innerHTML = recData.bpm + "(LPM)";
-    document.getElementById("temp_inst").innerHTML = roundToTwo(recData.temp) + "°C";
-    document.getElementById("spo_inst").innerHTML = recData.spo + "%";
-    document.getElementById("bf_inst").innerHTML = recData.bf + " BPM";
+    bpm.value = recData.bpm;
+    temp.value = recData.temp
+    spo.value = recData.spo;
+    bf.value = recData.bf
+    if (bpm.value >= 0){
+        document.getElementById("bpm_inst").innerHTML = bpm.value + "(LPM)";
+    }
+    else if (bpm.value < 0){
+        document.getElementById("bpm_inst").innerHTML = "-" + "(LPM)";
+    }
+
+    if (temp.value >= 0){
+        document.getElementById("temp_inst").innerHTML = roundToTwo(recData.temp) + "°C";
+    }
+    else if (temp.value < 0){
+        document.getElementById("temp_inst").innerHTML = "-" + "°C";
+    }
+
+    if (spo.value >= 0){
+        document.getElementById("spo_inst").innerHTML = spo.value + "°%";
+    }
+    else if (spo.value < 0){
+        document.getElementById("spo_inst").innerHTML = "-" + "%";
+    }
+
+    if (bf.value >= 0){
+        document.getElementById("bf_inst").innerHTML = bf.value + "BPM";
+    }
+    else if (bf.value < 0){
+        document.getElementById("bf_inst").innerHTML = "-" + "BPM";
+    }
+
+
 };
 
 function roundToTwo(num) {    
